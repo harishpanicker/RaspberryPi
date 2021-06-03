39e1c3474e06d47157cb00b599acd7fdb427ff53
harishpanicker <harishpanicker007@gmail.com>
2021-06-03 18:32:42 +0530

    Added pressure related logic and device file creation

--- i2c_client_driver.c
+++ i2c_client_driver.c
@@ -9,31 +9,34 @@
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/i2c.h>
+#include "i2c_sensor.h"
 
-#define I2C_ADDRESS 0x40 	//0x76
+#define I2C_ADDRESS 0x76
 #define I2C_NAME "i2c-slave"
 
 dev_t dev = 0;
 static struct class *i2c_class;
 static struct cdev i2c_cdev;
-struct i2c_client *bmp280_client = NULL;
-struct i2c_adapter *adap = NULL;
+static struct i2c_client *bmp280_client = NULL;
+static struct i2c_adapter *adap = NULL;
 
 
-/* Variables for temperature calculation */
-s32 dig_T1, dig_T2, dig_T3;
-
-/* Read current temperature from BMP280 sensor and return temperature in degree */
-
-s32 read_temperature(void) {
+/*
+ * Returns temperature in DegC, resolution is 0.01 DegC.  Output value of
+ * "5123" equals 51.23 DegC.  t_fine carries fine temperature as global
+ * value.
+ *
+ * Taken from datasheet, Section 3.11.3, "Compensation formula".
+ */
+s32 i2c_read_temperature(void) {
 	int var1, var2;
 	s32 raw_temp;
 	s32 d1, d2, d3;
 
 	/* Read Temperature */
-	d1 = i2c_smbus_read_byte_data(bmp280_client, 0xFA);
-	d2 = i2c_smbus_read_byte_data(bmp280_client, 0xFB);
-	d3 = i2c_smbus_read_byte_data(bmp280_client, 0xFC);
+	d1 = i2c_smbus_read_byte_data(bmp280_client, BMP280_REG_TEMP_MSB);
+	d2 = i2c_smbus_read_byte_data(bmp280_client, BMP280_REG_TEMP_LSB);
+	d3 = i2c_smbus_read_byte_data(bmp280_client, BMP280_REG_TEMP_XLSB);
 	raw_temp = ((d1<<16) | (d2<<8) | d3) >> 4;
 
 	/* Calculate temperature in degree */
@@ -41,6 +44,47 @@ s32 read_temperature(void) {
 
 	var2 = (((((raw_temp >> 4) - (dig_T1)) * ((raw_temp >> 4) - (dig_T1))) >> 12) * (dig_T3)) >> 14;
 	return ((var1 + var2) *5 +128) >> 8;
+}
+
+/*
+ * Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24
+ * integer bits and 8 fractional bits).  Output value of "24674867"
+ * represents 24674867/256 = 96386.2 Pa = 963.862 hPa
+ *
+ * Taken from datasheet, Section 3.11.3, "Compensation formula".
+ */
+
+u32 i2c_read_pressure(void) {
+
+	s64 var1, var2,p;
+	s64 raw_temp;
+	s64 d1, d2, d3;
+
+	/* Read Pressure */
+	d1 = i2c_smbus_read_byte_data(bmp280_client, BMP280_REG_PRESS_MSB);
+	d2 = i2c_smbus_read_byte_data(bmp280_client, BMP280_REG_PRESS_LSB);
+	d3 = i2c_smbus_read_byte_data(bmp280_client, BMP280_REG_PRESS_XLSB);
+	raw_temp = ((d1<<16) | (d2<<8) | d3) >> 4;
+
+	var1 = raw_temp - 128000;
+	var2 = var1 * var1 * dig_P6;
+	var2 += (var1 * dig_P5) << 17;
+	var2 += (dig_P4) << 35;
+	var1 = ((var1 * var1 * dig_P3) >> 8) +
+		((var1 * dig_P2) << 12);
+	var1 = ((((s64)1) << 47) + var1) * (dig_P1) >> 33;
+
+	if (var1 == 0)
+		return 0;
+
+	p = ((((s64)1048576 - raw_temp) << 31) - var2) * 3125;
+	p = div64_s64(p, var1);
+	var1 = ((dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+	var2 = ((dig_P8) * p) >> 19;
+	p = ((p + var1 + var2) >> 8) + ((dig_P7) << 4);
+
+	return (u32)p;
+
 }
 
 /*
@@ -67,14 +111,18 @@ static int i2c_release(struct inode *inode, struct file *file)
 static ssize_t i2c_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
 	int to_copy, not_copied, delta;
 	char out_string[20];
-	int temperature;
+	int temperature,pressure;
 
 	/* Get amount of bytes to copy */
 	to_copy = min(sizeof(out_string), count);
 
 	/* Get temperature */
-	temperature = read_temperature();
+	temperature = i2c_read_temperature();
 	snprintf(out_string, sizeof(out_string), "%d.%d\n", temperature/100, temperature%100);
+
+	/*Get pressure*/
+	pressure = i2c_read_pressure();
+	pr_info("pressure value = %d.%d\n",pressure/100, pressure%100);
 
 	/* Copy Data to user */
 	not_copied = copy_to_user(user_buffer, out_string, to_copy);
@@ -110,14 +158,42 @@ int bmp280_probe(struct i2c_client *i2c_client, const struct i2c_device_id *devi
 
 	pr_info("Probe called Successfully....\n");
 
-       	/* Read Chip ID */
+	/*Allocating Major number*/
+        if((alloc_chrdev_region(&dev, 0, 1, "I2c_Dev")) <0){
+                pr_err("Cannot allocate major number\n");
+                return -1;
+        }
+        pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+        /*Creating cdev structure*/
+        cdev_init(&i2c_cdev,&fops);
+
+        /*Adding character device to the system*/
+        if((cdev_add(&i2c_cdev,dev,1)) < 0){
+            pr_err("Cannot add the device to the system\n");
+            goto r_class;
+        }
+
+        /*Creating struct class*/
+        if((i2c_class = class_create(THIS_MODULE,"i2c_class")) == NULL){
+            pr_err("Cannot create the struct class\n");
+            goto r_class;
+        }
+
+        /*Creating device*/
+        if((device_create(i2c_class,NULL,dev,NULL,"i2c_device")) == NULL){
+            pr_err("Cannot create the Device 1\n");
+            goto r_device;
+        }
+        
+	/* Read Chip ID */
 	id = i2c_smbus_read_byte_data(bmp280_client, 0xD0);
 	printk("ID: 0x%x\n", id);
 
 	/* Read Calibration Values */
-	dig_T1 = i2c_smbus_read_word_data(bmp280_client, 0x88);
-	dig_T2 = i2c_smbus_read_word_data(bmp280_client, 0x8a);
-	dig_T3 = i2c_smbus_read_word_data(bmp280_client, 0x8c);
+	dig_T1 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_TEMP_CALIB1);
+	dig_T2 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_TEMP_CALIB2);
+	dig_T3 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_TEMP_CALIB3);
 
 	if(dig_T2 > 32767)
 		dig_T2 -= 65536;
@@ -125,11 +201,51 @@ int bmp280_probe(struct i2c_client *i2c_client, const struct i2c_device_id *devi
 	if(dig_T3 > 32767)
 		dig_T3 -= 65536;
 
+	/* Read Calibration Values for pressure */
+	dig_P1 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB1);
+	dig_P2 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB2);
+	dig_P3 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB3);
+	dig_P4 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB4);
+	dig_P5 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB5);
+	dig_P6 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB6);
+	dig_P7 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB7);
+	dig_P8 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB8);
+	dig_P9 = i2c_smbus_read_word_data(bmp280_client, BMP280_REG_PRESS_CALIB9);
+
+	if(dig_P2 > 32767)
+		dig_P2 -= 65536;
+
+	if(dig_P3 > 32767)
+		dig_P3 -= 65536;
+
+	if(dig_P4 > 32767)
+		dig_T2 -= 65536;
+
+	if(dig_P5 > 32767)
+		dig_P5 -= 65536;
+
+	if(dig_P6 > 32767)
+		dig_P6 -= 65536;
+
+	if(dig_P7 > 32767)
+		dig_P7 -= 65536;
+
+	if(dig_P8 > 32767)
+		dig_P8 -= 65536;
+
+	if(dig_P9 > 32767)
+		dig_P9 -= 65536;
+
 	/* Initialice the sensor */
 	i2c_smbus_write_byte_data(bmp280_client, 0xf5, 5<<5);
 	i2c_smbus_write_byte_data(bmp280_client, 0xf4, ((5<<5) | (5<<2) | (3<<0)));
 	return 0;
 
+r_device:
+        class_destroy(i2c_class);
+r_class:
+        unregister_chrdev_region(dev,1);
+        return -1;
 }
 
 static const struct i2c_device_id dev_id[]={
@@ -156,7 +272,8 @@ static struct i2c_board_info bmp280_i2c_board_info = {
 static int __init i2c_client_init(void)
 {
 	pr_info("Welcome to I2C Client Driver\n");
-	adap = i2c_get_adapter(6);
+	/*Bus number 1 according to raspberrypi board bus availability*/
+	adap = i2c_get_adapter(1);
 	if(adap == NULL)
 		printk("adapter error\n");
 	
@@ -177,9 +294,8 @@ static void __exit i2c_client_exit(void)
 	class_destroy(i2c_class);
 	cdev_del(&i2c_cdev);
 	unregister_chrdev_region(dev, 1);	
-	i2c_del_driver(&myclient_i2c);	
-	i2c_unregister_device(bmp280_client);    		
-	adap=NULL;
+	i2c_unregister_device(bmp280_client);
+    i2c_del_driver(&myclient_i2c);	
 	pr_info("client Module Removed ..successfully\n");
 
 }
