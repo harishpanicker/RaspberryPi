61e6670474edd0fefe52285fbde43f14e0a9d813
harishpanicker <harishpanicker007@gmail.com>
2021-06-09 18:34:43 +0530

    Added seperate function for date/time/day

--- i2c_ds3231_driver.c
+++ i2c_ds3231_driver.c
@@ -10,26 +10,90 @@
 #include <linux/device.h>
 #include <linux/i2c.h>
 #include <linux/ioctl.h>
+#include "rtc_header.h"
 
-#define I2C_ADDRESS 0x68
-#define I2C_NAME "i2c-slave"
-
-#define RD_DATE 	_IOR('a','a',int32_t*)
-#define RD_CALENDER 	_IOR('a','b',int32_t*)
-
+#define RD_TIME 	_IOR('a','a',int32_t*)
+#define RD_DATE 	_IOR('a','b',int32_t*)
+#define RD_DAY	 	_IOR('a','c',uint8_t*)
+#define MEM_SIZE	10  
 
 dev_t dev = 0;
 static struct class *i2c_class;
 static struct cdev i2c_cdev;
 static struct i2c_client *ds3231_client = NULL;
 static struct i2c_adapter *adap = NULL;
-int32_t value = 0;
+int32_t *value = NULL;
+uint8_t *kernel_buffer;
+
+/*
+ ** This function will be called when we to read the time
+ */
+static int Rtc_get_time(void){
+
+	u8 sec,min,hr;
+
+	sec = i2c_smbus_read_byte_data(ds3231_client, DS3231_REG_SECONDS);
+	min = i2c_smbus_read_byte_data(ds3231_client, DS3231_REG_MINUTES);
+	hr  = i2c_smbus_read_byte_data(ds3231_client, DS3231_REG_HOURS);
+	printk("hr:min:sec =  %x:%x:%x\n", hr, min, sec);
+
+	return hr*10000 + min*100 + sec;
+
+}
+
+/*
+ ** This function will be called when we want to read the date
+ */
+static int Rtc_get_date(void){
+
+	u8 dd,mm,yy;
+
+	dd  = i2c_smbus_read_byte_data(ds3231_client, DS3231_REG_DATE);
+	mm  = i2c_smbus_read_byte_data(ds3231_client, DS3231_REG_MONTH);
+	yy  = i2c_smbus_read_byte_data(ds3231_client, DS3231_REG_YEAR);
+
+	printk("dd/mm/yy =  %x/%x/%x\n", dd, mm, yy);
+
+	return dd*10000 + mm*100 + yy;
+
+}
+
+/*
+ ** This function will be called when we want to read which day today
+ */
+static uint8_t* Rtc_get_day(void){
+
+	u8 day;
+
+	day = i2c_smbus_read_byte_data(ds3231_client, DS3231_REG_DAY);
+
+	switch(day){
+		case 1 : return "MONDAY";
+		case 2 : return "TUESDAY";
+		case 3 : return "WEDNESDAY";
+		case 4 : return "THURSDAY";
+		case 5 : return "FRIDAY";
+		case 6 : return "SATURDAY";
+		case 7 : return "SUNDAY";
+	}
+	return 0;
+}
+
+//i2c_smbus_write_byte_data(ds3231_client, 0x00, );
 
 /*
  ** This function will be called when we open the Device file
  */
 static int i2c_open(struct inode *inode, struct file *file)
 {
+	if((value = kmalloc(sizeof(*value) , GFP_KERNEL)) == 0){
+		pr_info("Cannot allocate memory in kernel\n");
+		return -1;
+	}
+	if((kernel_buffer = kmalloc(MEM_SIZE , GFP_KERNEL)) == 0){
+		pr_info("Cannot allocate memory in kernel\n");
+		return -1;
+	}	
 	pr_info("Driver Open Function Called...!!!\n");
 	return 0;
 }
@@ -39,27 +103,10 @@ static int i2c_open(struct inode *inode, struct file *file)
  */
 static int i2c_release(struct inode *inode, struct file *file)
 {
+	kfree(kernel_buffer);
+	kfree(value);	
 	pr_info("Driver Release Function Called...!!!\n");
 	return 0;
-}
-/*
- ** This function will be called when we read the Device file
- */
-static ssize_t i2c_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
-
-	pr_info("Driver Read Function Called...!!!\n");
-	return 0;
-}
-
-
-/*
- ** This function will be called when we write the Device file
- */
-static ssize_t i2c_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
-{
-	pr_info("Driver Write Function Called...!!!\n");
-	//i2c_smbus_write_byte_data(ds3231_client, 0x00, );
-	return len;
 }
 
 /*
@@ -67,26 +114,27 @@ static ssize_t i2c_write(struct file *filp, const char __user *buf, size_t len, 
  */
 static long i2c_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-	u8 sec,min,hr;
 
 	switch(cmd) {
-		case RD_DATE:
-
-			sec = i2c_smbus_read_byte_data(ds3231_client, 0x00);
-			min = i2c_smbus_read_byte_data(ds3231_client, 0x01);
-			hr  = i2c_smbus_read_byte_data(ds3231_client, 0x02);
-			printk("hr:min:sec =  %d:%d:%d\n", hr, min, sec);
-			value = hr*10000 + min*100 + sec;
-			if( copy_to_user((int32_t*) arg, &value, sizeof(value)) )
+		case RD_TIME:
+			*value = Rtc_get_time();
+			if( copy_to_user((int32_t*) arg, value, sizeof(value)) )
 			{
-				pr_err("Data Read : Err!\n");
+				pr_err("Data Read for Time : Err!\n");
 			}
-			pr_info("Value = %d\n", value);
 			break;
-		case RD_CALENDER:
-			if( copy_to_user((int32_t*) arg, &value, sizeof(value)) )
+		case RD_DATE:
+			*value = Rtc_get_date();
+			if( copy_to_user((int32_t*) arg, value, sizeof(value)) )
 			{
-				pr_err("Data Read : Err!\n");
+				pr_err("Data Read for Calender : Err!\n");
+			}
+			break;
+		case RD_DAY:
+			kernel_buffer = Rtc_get_day();
+			if( copy_to_user((uint8_t*) arg, kernel_buffer, sizeof(kernel_buffer)) )
+			{
+				pr_err("Data Read for day : Err!\n");
 			}
 			break;
 		default:
@@ -99,8 +147,6 @@ static long i2c_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 static struct file_operations fops =
 {
 	.owner      = THIS_MODULE,
-	.read       = i2c_read,
-	.write      = i2c_write,
 	.open       = i2c_open,
 	.release    = i2c_release,
 	.unlocked_ioctl = i2c_ioctl,
