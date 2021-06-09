afceb4c1f883421fff9e9e0cf644c0d0cba37b80
harishpanicker <harishpanicker007@gmail.com>
2021-06-09 18:35:57 +0530

    register for ds3231 related macros

---
+++ rtc_header.h
@@ -0,0 +1,10 @@
+#define I2C_ADDRESS 0x68
+#define I2C_NAME "i2c-slave"
+
+#define DS3231_REG_SECONDS      0x00
+#define DS3231_REG_MINUTES      0x01
+#define DS3231_REG_HOURS        0x02
+#define DS3231_REG_DAY          0x03
+#define DS3231_REG_DATE         0x04
+#define DS3231_REG_MONTH        0x05
+#define DS3231_REG_YEAR         0x06
